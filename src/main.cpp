0ad31763a8ac582a3cc8a0d99c5d3b0b9f6d522d
Miroslav Demek <xdemek@fi.muni.cz>
2021-03-22 14:00:12 +0100

    Add json library

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 
 #include "parser.hpp"
+#include "json.hpp"
+
+namespace js = nlohmann;
 
 int main(int argc, char** argv) {
-	std::cout << "Hello CMake: " << return_five() << std::endl;
-	return 0;
+	js::json j;
+	j["something"] = 25;
+	j["pole"] = js::json::array();
+	j["pole"].push_back(5);
+	j["pole"].push_back("abs");
+	j["ab"] = js::json::array({ 10, 20, 30, "work?", js::json::object( { { "key", "val" } } ) });
+
+	std::cout << j << "\n";
 }
