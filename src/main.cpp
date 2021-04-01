06e20d6099a0bff5eecf5ec29e303d7ec3a4dce4
Miroslav Demek <xdemek@fi.muni.cz>
2021-04-01 13:32:46 +0200

    Add output to files + Add switch to specify output directory

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,79 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <string>
+#include <iomanip>
+
 
 #include "parser.hpp"
 #include "json.hpp"
 
 namespace js = nlohmann;
 
+std::string get_filename_from_path(const std::string& path) {
+	auto n = path.rfind('/');
+	if (n != std::string::npos) {
+		return path.substr(n + 1, path.size() - n);
+	}
+	n = path.rfind('\\');
+	if (n != std::string::npos) {
+		return path.substr(n + 1, path.size() - n);
+	}
+	return path;
+}
 
+void replace_suffix(std::string& file, const std::string& new_suffix) {
+	auto n = file.rfind('.');
+	if (n != std::string::npos) {
+		file.erase(n);
+	}
+	file.append(new_suffix);
+}
+
+std::string get_output_filename(const std::string out_path, const std::string& in_filename) {
+	std::string path = out_path.empty() ? "" : out_path + "/";
+	auto file = get_filename_from_path(in_filename);
+	replace_suffix(file, ".json");
+	return path + file;
+}
 
 int main(int argc, char** argv) {
-	/* 
-	// Example json 
-	js::json j;
-	j["something"] = 25;
-	j["pole"] = js::json::array();
-	j["pole"].push_back(5);
-	j["pole"].push_back("abs");
-	j["ab"] = js::json::array({ 10, 20, 30, "work?", js::json::object( { { "key", "val" } } ) });
-	*/
+	std::vector<std::string> input_files;
+	std::string out_path = "";
+
+	for (int i = 1; i < argc; ++i) {
+		std::string arg { argv[i] };
+		if (arg == "-d") {
+			if (i + 1 >= argc) {
+				std::cerr << "Error: -d requires an argument\n";
+				std::exit(1);
+			}
+			++i;
+			out_path = argv[i];
+		} else {
+			input_files.push_back(argv[i]);
+		}
+	}
 	
-	if (argc > 1) {
-		// go through all the arguments, assume they are files and parse them
-		for (int i = 1; i < argc; ++i) {
-			std::ifstream file{ argv[i] };
-			if (!file) {
-				std::cerr << "Failed to open file: " << argv[i] << "\n";
+	if (input_files.size() > 0) {
+		for (const auto& filename : input_files) {
+			std::ifstream in_file{ filename };
+			if (!in_file) {
+				std::cerr << "Failed to open file: " << filename << "\n";
 				continue;
 			}
-			auto j = parse_document(file);
-			std::cout << j.dump(4) << "\n";
+			std::string out_filename =  get_output_filename(out_path, filename); 
+			std::ofstream out_file{ out_filename };
+			if (!out_file) {
+				std::cerr << "Failed to create file: " << out_filename << "\n";
+				continue;
+			}
+			auto j = parse_document(in_file);
+			out_file << std::setw(4) << j << std::endl;
+			//std::cout << j.dump(4) << "\n";
 		}
-	} else { // no arguments -> take input from std::cin
-		auto j = parse_document(std::cin);
-		std::cout << j.dump(4) << "\n";
+	} else { // no input files -> take input from std::cin
+		//auto j = parse_document(std::cin);
+		//std::cout << j.dump(4) << "\n";
 	}
 }
