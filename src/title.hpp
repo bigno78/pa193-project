d62270e1b15ac282fc9768857e84fe3a4278e94e
Kristýna Demková <xdemkova@fi.muni.cz>
2021-04-09 17:06:22 +0200

    parsing of certificates with security target line

--- src/title.hpp
+++ src/title.hpp
@@ -3,7 +3,8 @@
 #include <string>
 #include "utils.hpp"
 
-std::string parse_title(std::vector<std::string> data) {
+
+std::string parse_NSCIB(std::vector<std::string>& data) {
 	if (data[0].find("Rheinland Nederland B.V.") != std::string::npos) {
 		std::vector<std::string> lines = {};
 		for (size_t i = 11; i <= 16; i++) {
@@ -11,12 +12,65 @@ std::string parse_title(std::vector<std::string> data) {
 				break;
 			}
 			lines.push_back(data[i]);
-			trim(lines[i - 11]);
+			trim(lines[lines.size()-1]);
 		}
 		std::string title = lines[0];
 		for (size_t i = 1; i < lines.size(); i++) {
 			append_line(title, lines[i]);
 		}
+		return title;
+	}
+	return "";
+}
+
+std::string parse_security_target(std::vector<std::string>& data) {
+	for (int i = 0; i < 10; i++) {
+		std::string line = data[i];
+		std::transform(line.begin(), line.end(), line.begin(), [](unsigned char c) { return std::tolower(c); });
+		std::vector<std::string> lines = {};
+		if (line.find("security target") != std::string::npos) {
+			if (i == 0) {
+				size_t from = i + 1;
+				if (data[from].empty()) {
+					from++;
+				}
+				for (size_t j = from; j < i + 6; j++) {
+					if (data[j].empty()) {
+						break;
+					}
+					lines.push_back(data[j]);
+					trim(lines[lines.size() - 1]);
+				}
+				std::string title = lines[0];
+				for (size_t j = 1; j < lines.size(); j++) {
+					append_line(title, lines[j]);
+				}
+				return title;
+			}
+			else {
+				for (int j = i - 1; j > i - 5; j--) {
+					if (j < 0 || data[j].empty()) {
+						break;
+					}
+					lines.push_back(data[j]);
+					trim(lines[lines.size() - 1]);
+				}
+				std::string title = lines[lines.size()-1];
+				for (int j = lines.size()-2; j >= 0; j--) {
+					append_line(title, lines[j]);
+				}
+				return title;
+			}
+		}
+	}
+	return "";
+}
+
+std::string parse_title(std::vector<std::string> &data) {
+	if (std::string title = parse_NSCIB(data); !title.empty()) {
+		return title;
+	}
+	if (std::string title = parse_security_target(data); !title.empty()) {
 		return title;
 	}
 	std::vector<std::string> candidets = {};
