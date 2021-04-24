e1b218c504d21396fd21531600e3f59ba747499a
Miroslav Demek <xdemek@fi.muni.cz>
2021-04-24 23:59:03 +0200

    Cosmetic changes

--- src/contents.hpp
+++ src/contents.hpp
@@ -6,6 +6,10 @@ struct TocItem {
     int page;
 };
 
+/**
+ * Return the next index after and including i that is not a space.
+ * If there is no such index return line.size()
+ */
 size_t ignore_whitespace(const std::string& line, size_t i) {
     while (i < line.size() && is_space(line[i])) {
         ++i;
@@ -13,8 +17,77 @@ size_t ignore_whitespace(const std::string& line, size_t i) {
     return i;
 }
 
+/**
+ * Return the next index after and including i that contains a space.
+ * If there is no such index return line.size()
+ */
+size_t next_whitespace(const std::string& line, size_t i) {
+    while (i < line.size() && !is_space(line[i])) {
+        ++i;
+    }
+    return i;
+}
+
+size_t find_chapter_num_end(const std::string& line, size_t i) {
+    assert(line.size() > i);
+
+    if (is_digit(line[i])) {
+        size_t pos = next_whitespace(line, i);
+        return pos;
+    } else if (is_upper(line[i])) {
+        if (i+1 < line.size() && is_space(line[i+1])) {
+            return i+1;
+        }
+        if (i+2 < line.size() && line[i+1] == '.' && is_space(line[i+2])) {
+            return i+2;
+        }
+    }    
+
+    return i;
+}
+
+size_t find_chapter_name_end(const std::string& line, size_t i) {
+    size_t pos = i;
+
+    size_t mezera_count = 0; // number of consecutive spaces
+    size_t dot_count = 0; // number of consecutive dots
+
+    while (pos < line.size() && dot_count < 2 && mezera_count < 2) {
+        if (is_space(line[pos])) {
+            mezera_count++;
+        } else if (line[pos] == '.') {
+            dot_count++;
+        } else {
+            mezera_count = 0;
+            dot_count = 0;
+        }
+        pos++;
+    }
+
+    if (dot_count == 2) {
+        pos -= 2;
+    }
+
+    return pos;
+}
+
+size_t ignore_padding(const std::string& line, size_t i) {
+    while (i < line.size() && (line[i] == '.' || is_space(line[i]))) {
+        ++i;    
+    }
+    return i;
+}
+
+size_t find_page_num_end(const std::string& line, size_t i) {
+    while (i < line.size() && is_digit(line[i])) {
+        ++i;
+    }
+    return i;
+}
+
+
+
 nlohmann::json parse_contents(std::vector<std::string>& data) {
-    nlohmann::json toret = nlohmann::json::array({});
     std::vector<TocItem> contents;
     size_t i = 0;
     static const std::regex reg(R"(content|index)", std::regex_constants::icase);
@@ -31,10 +104,6 @@ tryagain:
         }
     }
 
-    /*if (i >= data.size()) {
-        return {};
-    }*/
-
     constexpr int max_tolerance = 10;
     int tolerance = max_tolerance;
 
@@ -43,14 +112,12 @@ tryagain:
     std::string page;
     
     std::string line;
-    while (1) {
+    
+    while (i < data.size() && tolerance > 0) {
         chapter_num.clear();
         chapter_name.clear();
         page.clear();
 
-        if (i >= data.size() || tolerance <= 0) {
-            break;
-        }
         if (is_empty_line(data[i])) {
             i++;
             continue;
@@ -63,55 +130,34 @@ tryagain:
 
         line = data[i];
         trim(line);
-        std::cout << "tolerance: " << tolerance << std::endl;
-        std::cout << line << std::endl;
-        size_t pos1 = 0;
-        if (is_digit(line[0])) {
-            pos1 = line.find(' ');
-            if (pos1 == std::string::npos) {
-                i++;
-                tolerance--;
-                continue;
-            }
-            chapter_num = line.substr(0, pos1);
-            trim(chapter_num);
-        } else if ( line.size() >= 3 && is_upper(line[0]) && line[1] == '.' && is_space(line[2])) {
-            pos1 = 2;
+        //std::cout << "tolerance: " << tolerance << std::endl;
+        //std::cout << line << std::endl;
+        size_t pos1 = find_chapter_num_end(line, 0);
+        if (pos1 >= line.size()) {
+            i++;
+            tolerance--;
+            continue;
+        }
+        if (pos1 > 0 && line[pos1-1] == '.') {
+            chapter_num = line.substr(0, pos1-1);    
+        } else {
             chapter_num = line.substr(0, pos1);
         }
 
         pos1 = ignore_whitespace(line, pos1);
         if (pos1 >= line.size() || line[pos1] == '.') {
-            std::cout << "1" << std::endl;
             i++;
             tolerance--;
             continue;
         }
         
-        size_t pos2 = pos1;
-        size_t mezera_count = 0;
-        size_t dot_count = 0;
-        while (pos2 < line.size() && dot_count < 2 && mezera_count < 2) {
-            if (is_space(line[pos2])) {
-                ++mezera_count;
-            } else if (line[pos2] == '.') {
-                dot_count++;
-            } else {
-                mezera_count = 0;
-                dot_count = 0;
-            }
-            ++pos2;
-        }
-        if (dot_count == 2) {
-            pos2 -= 2;
-        }
-
+        size_t pos2 = find_chapter_name_end(line, pos1);
         if (pos2 >= line.size()) {
-            std::cout << "2" << std::endl;
             i++;
             tolerance--;
             continue;
         }
+        chapter_name = line.substr(pos1, pos2 - pos1);
 
         /*auto pos2 = line.find('.', pos1);
         if (pos2 == std::string::npos) {
@@ -139,26 +185,14 @@ tryagain:
             }
         }*/
 
-        chapter_name = line.substr(pos1, pos2 - pos1);
-        trim(chapter_name);
-
-        while (pos2 < line.size() &&
-               (line[pos2] == '.' || is_space(line[pos2]))) {
-            ++pos2;    
-        }
-
-        size_t pos3 = pos2;
-        while (pos3 < line.size() && is_digit(line[pos3])) {
-            ++pos3;
-        }
+        pos2 = ignore_padding(line, pos2);
+        size_t pos3 = find_page_num_end(line, pos2);
         if (pos3 > line.size()) {
-            std::cout << "3" << std::endl;
             i++;
             tolerance--;
             continue;
         }
         if (pos3 <= line.size()-1 && !is_space(line[pos3])) {
-            std::cout << "4" << std::endl;
             i++;
             tolerance--;
             continue;
@@ -194,16 +228,16 @@ tryagain:
         if (chapter_name.empty() || page.empty()) {
             i++;
             tolerance--;
-            continue;
+            break;
         }
 
         try {
             contents.push_back( {chapter_num, chapter_name, std::stoi(page)});
         } catch (...) {
-            assert(false);
+            assert(false); // this should not happen
         }
         tolerance = max_tolerance;
-        i++;
+        i++;    
     }
     
     if (i < data.size() && contents.size() < 5) {
@@ -211,6 +245,7 @@ tryagain:
         goto tryagain;
     }
     
+    nlohmann::json toret = nlohmann::json::array({});
     for (size_t i = 0; i < contents.size(); i++) {
         //std::cout << std::get<1>(contents[i]) << std::endl;
         toret.push_back(nlohmann::json::array({ 
