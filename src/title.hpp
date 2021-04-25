4ecd704f603c36284bf2887977276d09d40e765d
Miroslav Demek <xdemek@fi.muni.cz>
2021-04-25 22:04:12 +0200

    undo a reset

--- src/title.hpp
+++ src/title.hpp
@@ -6,9 +6,47 @@
 #include "utils.hpp"
 
 
+//concatenates all strings in vector into a single string
+std::string multiple_lines_into_one(std::vector<std::string>& lines) {
+    std::string title = lines[0];
+    for (size_t i = 1; i < lines.size(); i++) {
+        append_line(title, lines[i]);
+    }
+    return title;
+}
+
+//concatenates all strings in vector into a single string starting from the last one
+std::string multiple_lines_into_one_reverse(std::vector<std::string>& lines) {
+    std::string title = lines[lines.size() - 1];
+    for (int j = static_cast<int>(lines.size() - 2); j >= 0; j--) {
+        append_line(title, lines[j]);
+    }
+    return title;
+}
+
+//creates new candidate for title out of current
+void add_new_candidate(std::vector<std::string>& candidates, std::vector<std::string>& current) {
+    std::string candidet = current[0];
+    trim(candidet);
+    for (size_t j = 1; j < current.size(); j++) {
+        trim(current[j]);
+        append_line(candidet, current[j]);
+    }
+    if (!candidet.empty()) {
+        candidates.push_back(candidet);
+    }
+    current.clear();
+}
+
+//function for parsing the type of certificate which has Rheinland Nederland B.V. in the first line
 std::string parse_NSCIB(const std::vector<std::string>& data) {
 	if (data[0].find("Rheinland Nederland B.V.") != std::string::npos) {
 		std::vector<std::string> lines = {};
+        if (data.size() <= 17) {
+            return "";
+		}
+		//for types of certificates containing Rheinland Nederland B.V. in the first line
+		//it is typical that title is on line 11 (or 11 + some more if title is multi line)
 		for (size_t i = 11; i <= 16; i++) {
 			if (data[i].empty()) {
 				break;
@@ -16,28 +54,29 @@ std::string parse_NSCIB(const std::vector<std::string>& data) {
 			lines.push_back(data[i]);
 			trim(lines[lines.size()-1]);
 		}
-		std::string title = lines[0];
-		for (size_t i = 1; i < lines.size(); i++) {
-			append_line(title, lines[i]);
-		}
-		return title;
+        return multiple_lines_into_one(lines);
 	}
 	return "";
 }
 
+//function for parsing certificates which have security target somewhere in the first 10 lines
 std::string parse_security_target(const std::vector<std::string>& data) {
+    if (data.size() <= 10) {
+        return "";
+	}
 	for (size_t i = 0; i < 10; i++) {
 		std::string line = data[i];
-		//std::transform(line.begin(), line.end(), line.begin(), [](unsigned char c) { return std::tolower(c); });
         to_lower(line);
 		std::vector<std::string> lines = {};
 		if (line.find("security target") != std::string::npos) {
+			//case where title is after "security target"
 			if (i == 0) {
 				size_t from = i + 1;
 				if (data[from].empty()) {
 					from++;
 				}
-				for (size_t j = from; j < i + 6; j++) {
+				//lets assume title does not have more than 10 lines
+				for (size_t j = from; j < i + 10; j++) {
 					if (data[j].empty() || data[j].find(".....") != std::string::npos) {
 						break;
 					}
@@ -47,17 +86,15 @@ std::string parse_security_target(const std::vector<std::string>& data) {
 				if (lines.empty()) {
 					return "";
 				}
-				std::string title = lines[0];
-				for (size_t j = 1; j < lines.size(); j++) {
-					append_line(title, lines[j]);
-				}
-				return title;
+				return multiple_lines_into_one(lines);
 			}
+			//case where title is after "security target"
 			else {
 				int from = 1;
-				if (data[(size_t)i - 1].empty()) {
+				if (data[static_cast<size_t>(i - 1)].empty()) {
 					from++;
 				}
+				//lets assume title does not have more than 10 lines
 				for (int j = static_cast<int>(i - from); j > i - 10; j--) {
 					if (j < 0 || data[j].empty() || data[j].find(".....") != std::string::npos) {
 						break;
@@ -68,11 +105,7 @@ std::string parse_security_target(const std::vector<std::string>& data) {
 				if (lines.empty()) {
 					return "";
 				}
-				std::string title = lines[lines.size()-1];
-				for (int j = static_cast<int>(lines.size()-2); j >= 0; j--) {
-					append_line(title, lines[j]);
-				}
-				return title;
+				return multiple_lines_into_one_reverse(lines);
 			}
 		}
 	}
@@ -80,19 +113,28 @@ std::string parse_security_target(const std::vector<std::string>& data) {
 }
 
 std::string parse_title(const std::vector<std::string> &data) {
+    if (data.size() == 0) {
+        return "";
+	}
 	if (std::string title = parse_NSCIB(data); !title.empty()) {
 		return title;
 	}
 	if (std::string title = parse_security_target(data); !title.empty()) {
 		return title;
 	}
-	std::vector<std::string> candidets = {};
+	std::vector<std::string> candidates = {};
 	std::vector<std::string> current = {};
-	for (size_t i = 0; i < 50; i++) {
+	//number of lines in which we assume we will find title
+    size_t length = 50;
+    if (length > data.size()) {
+        length = data.size();
+	}
+	for (size_t i = 0; i < length; i++) {
 		std::string line = data[i];
 		trim(line);
+		//title lines are short - skip long lines
 		if (line.length() > 60) {
-			for (size_t j = i + 1; j < 50; j++) {
+			for (size_t j = i + 1; j < length; j++) {
 				if (!is_empty_line(data[j])) {
 					continue;
 				}
@@ -102,42 +144,28 @@ std::string parse_title(const std::vector<std::string> &data) {
 			}
 		}
 		if (is_empty_line(data[i]) && !current.empty()) {
-			std::string candidet = current[0];
-			trim(candidet);
-			for (size_t j = 1; j < current.size(); j++) {
-				trim(current[j]);
-				append_line(candidet, current[j]);
-			}
-			if (!candidet.empty()) {
-				//std::cout << candidet << std::endl;
-				candidets.push_back(candidet);
-			}
-			current.clear();
+            add_new_candidate(candidates, current);
 			continue;
 		}
 		current.push_back(data[i]);
 	}
-	for (size_t i = 0; i < candidets.size(); i++) {
-		std::vector<Column> temp = split_line_into_columns(candidets[i]);
+	//lines with multiple collums are not containing title
+	for (size_t i = 0; i < candidates.size(); i++) {
+		std::vector<Column> temp = split_line_into_columns(candidates[i]);
 		if (temp.size() > 1) {
-			candidets.erase(candidets.begin() + i);
+			candidates.erase(candidates.begin() + i);
 			i--;
 		}
 	}
 
-	for (size_t i = 0; i < candidets.size(); i++) {
-		std::cout << candidets[i] << ": " << candidets[i].length() << std::endl;
-	}
-	//breaks stuff too
-	bool (*lambda)(std::string & a, std::string & b) = [](std::string &a, std::string &b) {return a.length() < b.length(); };
-
-	/**
-	if (data[0].find("Rheinland Nederland B.V.") != std::string::npos) {
-		lambda = [](std::string &a, std::string &b) {return a.length() > b.length(); };
-	}
-	**/
-	auto it = (std::max_element(candidets.begin(), candidets.end(), lambda));
-	if (it == candidets.end() || (*it).empty()) {
+	//the longest candidet is assumed to be title
+    auto it = (std::max_element(candidates.begin(), candidates.end(),
+                                    [](std::string& a, std::string& b) { return a.length() < b.length(); }));
+	if (it == candidates.end() || (*it).empty()) {
+        if (data.empty()) {
+            return "";
+		}
+		//if all else fails, assume that first line is title
 		std::string line = data[0];
 		trim(line);
 		return line;
